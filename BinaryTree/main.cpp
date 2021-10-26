5995f7ac9787f913adcc3616308e20ec8c5bf871
Jakos <jakosvadim@gmail.com>
2021-10-26 12:42:48 +0300

    Added unit tests for addNode, deleteNode, Iterator

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
 class Tree;
 class TreeIterator;
@@ -19,6 +20,7 @@ public:
     const Node* getParent() const { return parent; }
 
     std::string name;
+    std::string description;
 
     Node() : name(""), left(nullptr), right(nullptr), parent(nullptr) {}
     Node(const std::string& _name, Node* _parent = nullptr)  
@@ -58,6 +60,9 @@ public:
 
     Node& operator*() { return *node; }
     const Node& operator*() const { return *node; }
+
+    Node* operator->() { return node; }
+    const Node* operator->() const { return node; }
     
     TreeIterator& operator++() {
         if (node->right) {
@@ -121,7 +126,12 @@ private:
 
     void smallTurnLeft(Node* a, Node* b) {
         if (a->parent) {
-            a->parent->right = b;
+            if (a->parent->left == a) {
+                a->parent->left = b;
+            }
+            else {
+                a->parent->right = b;
+            }
         }
         b->parent = a->parent;
         a->parent = b;
@@ -134,7 +144,12 @@ private:
 
     void smallTurnRight(Node* a, Node* b) {
         if (a->parent) {
-            a->parent->left = b;
+            if (a->parent->left == a) {
+                a->parent->left = b;
+            }
+            else {
+                a->parent->right = b;
+            }
         }
         b->parent = a->parent;
         a->parent = b;
@@ -371,38 +386,192 @@ public:
         return TreeIterator();
     }
 };
+
+void testAddNode() {
+    Tree *result = new Tree();
+
+    auto abc = result->addNode("Abc");
+    assert(abc != nullptr);
+    assert(abc->name == "Abc");
+    assert(abc->description == "");
+    assert(abc->getParent() == nullptr);
+    assert(abc->getLeft() == nullptr);
+    assert(abc->getRight() == nullptr);
+
+    auto def = result->addNode("Def");
+    assert(def != nullptr);
+    assert(def->name == "Def");
+    assert(def->description == "");
+    assert(def->getParent() == abc);
+    assert(abc->getRight() == def);
+
+    // small turn left 
+    auto ghi = result->addNode("Ghi");
+    assert(ghi != nullptr);
+    assert(ghi->name == "Ghi");
+    assert(ghi->description == "");
+    assert(ghi->getParent() == def);
+    assert(ghi->getLeft() == nullptr);
+    assert(ghi->getRight() == nullptr);
+    assert(def->getParent() == nullptr);
+    assert(def->getLeft() == abc);
+    assert(def->getRight() == ghi);
+    assert(abc->getParent() == def);
+    assert(abc->getLeft() == nullptr);
+    assert(abc->getRight() == nullptr);
+
+    delete result;
+
+    // small turn right 
+    result = new Tree();
+    auto a = result->addNode("a");
+    assert(a != nullptr);
+    assert(a->name == "a");
+    assert(a->description == "");
+    assert(a->getParent() == nullptr);
+    assert(a->getLeft() == nullptr);
+    assert(a->getRight() == nullptr);
+
+    auto b = result->addNode("b");
+    assert(b != nullptr);
+    assert(b->name == "b");
+    assert(b->description == "");
+    assert(b->getParent() == a);
+    assert(b->getLeft() == nullptr);
+    assert(b->getRight() == nullptr);
+
+    auto c = result->addNode("c");
+    assert(c != nullptr);
+    assert(c->name == "c");
+    assert(c->description == "");
+    assert(c->getParent() == b);
+    assert(c->getLeft() == nullptr);
+    assert(c->getRight() == nullptr);
+    assert(b->getParent() == nullptr); 
+    assert(b->getLeft() == a);
+    assert(b->getRight() == c);
+    assert(a->getParent() == b);
+    assert(a->getLeft() == nullptr);
+    assert(a->getRight() == nullptr);
+
+    // big turn left 
+    result = new Tree();
+    abc = result->addNode("Abc");
+    assert(abc != nullptr);
+    assert(abc->name == "Abc");
+    assert(abc->description == "");
+    assert(abc->getParent() == nullptr);
+    assert(abc->getLeft() == nullptr);
+    assert(abc->getRight() == nullptr);
+
+    auto ade = result->addNode("Ade");
+    assert(ade != nullptr);
+    assert(ade->name == "Ade");
+    assert(ade->description == "");
+    assert(ade->getParent() == abc);
+    assert(ade->getRight() == nullptr);
+    assert(ade->getLeft() == nullptr);
+
+    auto acd = result->addNode("Acd");
+    assert(acd != nullptr);
+    assert(acd->name == "Acd");
+    assert(acd->description == "");
+    assert(acd->getParent() == nullptr);
+    assert(ade->getRight() == nullptr);
+    assert(ade->getParent() == acd);
+    assert(ade->getLeft() == nullptr);
+    assert(abc->getLeft() == nullptr);
+    assert(abc->getParent() == acd);
+    assert(abc->getRight() == nullptr);
+
+    delete result;
+
+    // Big turn right 
+    result = new Tree();
+    abc = result->addNode("Abc");
+    assert(abc != nullptr);
+    assert(abc->name == "Abc");
+    assert(abc->description == "");
+    assert(abc->getParent() == nullptr);
+    assert(abc->getLeft() == nullptr);
+    assert(abc->getRight() == nullptr);
+
+    acd = result->addNode("Acd");
+    assert(acd != nullptr);
+    assert(acd->name == "Acd");
+    assert(acd->description == "");
+    assert(acd->getParent() == abc);
+    assert(acd->getRight() == nullptr);
+    assert(acd->getLeft() == nullptr);
+
+    ade = result->addNode("Ade");
+    assert(ade != nullptr);
+    assert(ade->name == "Ade");
+    assert(ade->description == "");
+    assert(ade->getParent() == acd);
+    assert(ade->getRight() == nullptr);
+    assert(ade->getLeft() == nullptr);
+    assert(abc->getParent() == acd);
+    assert(abc->getLeft() == nullptr);
+    assert(abc->getRight() == nullptr);
+    assert(acd->getParent() == nullptr);
+    assert(acd->getRight() == ade);
+    assert(acd->getLeft() == abc);
+}
+
+void testDeleteNode() {
+    Tree* result = new Tree();
+    for (int i = 0; i < 26; i++) {
+        result->addNode(std::string(1, 'a' + i));
+    }
+    Node* deletedNode = result->findNode("z");
+    assert(deletedNode != nullptr);
+    result->deleteNode(deletedNode);
+    for (auto it = result->begin(); it != result->end(); it++) {
+        assert(it->name != "z");
+        if (it->getLeft() != nullptr) {
+            assert(it->name > it->getLeft()->name);
+        }
+        if (it->getRight() != nullptr) {
+            assert(it->name < it->getRight()->name);
+        }
+    }
+    for (int i = 0; i < 25; i++) {
+        std::string s(1, 'a' + i);
+        Node* deletedNode = result->findNode(s);
+        assert(deletedNode != nullptr);
+        result->deleteNode(deletedNode);
+        for (auto it = result->begin(); it != result->end(); it++) {
+            assert(it->name != s);
+            if (it->getLeft() != nullptr) {
+                assert(it->name > it->getLeft()->name);
+            }
+            if (it->getRight() != nullptr) {
+                assert(it->name < it->getRight()->name);
+            }
+        }
+    }
+    assert(result->begin() == nullptr);
+    result->addNode("z");
+    assert(result->begin()->name == "z");
+}
+
+void testIterator() {
+    Tree* result = new Tree();
+    for (int i = 0; i < 26; i++) {
+        result->addNode(std::string(1, 'a' + i));
+    }
+    char tempElement = 'a';
+    for (auto it = result->begin(); it != result->end(); it++) {
+        assert(std::string(1, tempElement) == it->name);
+        tempElement++;
+    }
+}
  
 int main() {
-    Tree bst;
-    for (int i = 0; i < 5; i++) {
-        bst.addNode(std::to_string(i));
-    }
-
-    for (auto it = bst.begin(); it != bst.end(); it++) {
-        Node newNode = *it;
-        std::cout << "Hello, I am " << newNode.name << std::endl;
-        if (newNode.getParent())
-            std::cout << "parent " << newNode.getParent()->name << std::endl;
-        if (newNode.getLeft())
-            std::cout << "left " << newNode.getLeft()->name << std::endl;
-        if (newNode.getRight())
-            std::cout << "right " << newNode.getRight()->name << std::endl;
-        std::cout << std::endl;
-    }
-    Node* nodeToDelete = bst.findNode("3");
-    std::cout << "Delete " << nodeToDelete->name << std::endl;
-    bst.deleteNode(nodeToDelete);
-    for (auto it = bst.begin(); it != bst.end(); it++) {
-        Node newNode = *it;
-        std::cout << "Hello, I am " << newNode.name << std::endl;
-        if (newNode.getParent())
-            std::cout << "parent " << newNode.getParent()->name << std::endl;
-        if (newNode.getLeft())
-            std::cout << "left " << newNode.getLeft()->name << std::endl;
-        if (newNode.getRight())
-            std::cout << "right " << newNode.getRight()->name << std::endl;
-        std::cout << std::endl;
-    }
+    testAddNode();
+    testDeleteNode();
+    testIterator();
 
     return 0;
 }
