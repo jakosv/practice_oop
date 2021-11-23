ab7e6fae9511da7a768459a3ab83292e131acf93
Jakos <jakosvadim@gmail.com>
2021-11-23 18:35:39 +0300

    Bug fixed

--- GraphPathFinder/Ford-Bellman/ford-bellman.cpp
+++ GraphPathFinder/Ford-Bellman/ford-bellman.cpp
@@ -10,13 +10,12 @@ extern "C" float shortest_length(graph& g, const std::string& src,
         const std::string& dst)
 {
     const int INF = 1e9;
-    vector<map<string, float>> dist(g.size());
-    for (int k = 0; k < g.size(); k++) {
-        for (auto it = g.begin(); it != g.end(); it++) {
-            dist[k][it->first] = INF;
-        }
+    //vector<map<string, float>> dist(g.size());
+    map<string, float> dist;
+    for (auto it = g.begin(); it != g.end(); it++) {
+        dist[it->first] = INF;
     }
-    dist[0][src] = 0;
+    dist[src] = 0;
 
     for (size_t k = 0; k < g.size() - 1; k++) {
         for (auto it = g.begin(); it != g.end(); it++) {
@@ -24,17 +23,12 @@ extern "C" float shortest_length(graph& g, const std::string& src,
             for (int j = 0; j < it->second.size(); j++) {
                 string u = it->second[j].first;
                 float w = it->second[j].second;
-                if (dist[k][u] != INF) {
-                    dist[k+1][v] = min(dist[k+1][v], dist[k][u] + w);
+                if (dist[u] != INF) {
+                    dist[v] = min(dist[v], dist[u] + w);
                 }
             }
         }
     }
 
-    float ans = INF;
-    for (size_t k = 0; k < g.size(); k++) {
-        ans = min(ans, dist[k][dst]);
-    }
-
-    return ans;
+    return dist[dst];
 }
